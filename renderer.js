f231f0d533794c0ccf3fceebaebe4dcafa9fd8a8
gabrielseaver9678 <gabrielseaver9678@gmail.com>
2022-08-11 23:29:31 -0400

    basic interlinear text display

--- renderer.js
+++ renderer.js
@@ -1,10 +1,38 @@
 const inputText = document.getElementById("input-text")
 const translateButton = document.getElementById("translate-button")
+const interlinearView = document.getElementById("interlinear-view")
 
 translateButton.onclick = translateInput
 
 async function translateInput () {
-    alert(await electronAPI.translate("ru", "en", inputText.value.toString()))
+    interlinearView.innerHTML = ""
+    
+    const wordPairs = await getTranslatedWordPairs("ru", "en", inputText.value.toString())
+    console.log(wordPairs)
+    wordPairs.forEach(wordPair => {
+        interlinearView.appendChild(wordPairToElem(wordPair))
+        const spaceChar = document.createElement("span")
+        spaceChar.textContent = " "
+        interlinearView.appendChild(spaceChar)
+    });
+}
+
+function wordPairToElem (wordPair) {
+    const wordPairElem = document.createElement("span")
+    wordPairElem.className = "wordPairElem"
+    
+    const sourceWordElem = document.createElement("span")
+    sourceWordElem.textContent = wordPair[0]
+    sourceWordElem.className = "sourceLangWord"
+    
+    const targetWordElem = document.createElement("span")
+    targetWordElem.textContent = wordPair[1]
+    targetWordElem.className = "targetLangWord"
+    
+    wordPairElem.appendChild(sourceWordElem)
+    wordPairElem.appendChild(targetWordElem)
+    
+    return wordPairElem
 }
 
 function getWords (text) {
